96fdb6e1a4c781e82f786b215c6b7f647b040622
ISCMSHY <sungwhan4854@naver.com>
2023-04-06 13:31:06 +0900

    RSA_Code

--- RSA_func.h
+++ RSA_func.h
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 #include <math.h>
 
-char hex_to_char(int hex);
-long long EA(long long a, long long b);
-long long EEA(long long a, long long b);
-void get_e(long long phi_N, long long* e_list);
-int euler_seive(int N, int* prime);
-int prime_F(int N, int* prime, int* pow_a, int idx);
-int euler_p(int N);
-int F(int N);
-void create_RSA_key(long long p, long long q, long long *e, long long *d);
-long long encrypt(long long plaintext, long long N, long long e);
-long long decrypt(long long ciphertext, long long N, long long d);
+char hex_to_char(int hex); // change hex to char
+long long EA(long long a, long long b); // Euclidean Algorithm
+long long EEA(long long a, long long b); // Extended Euclidean Algorithm
+void get_e(long long phi_N, long long* e_list); // get All e(public key) that satisfied gcd(phi_N, e) = 1
+int euler_seive(int N, int* prime); // euler_seive algorithm
+int prime_F(int N, int* prime, int* pow_a, int idx); // prime Factorization
+int euler_p(int N); // euler pi function algorithm
+int F(int N); // integer Factorization
+void create_RSA_key(long long p, long long q, long long *e, long long *d); // Generating RSA Key
+long long encrypt(long long plaintext, long long N, long long e); // RSA Encryption
+long long decrypt(long long ciphertext, long long N, long long d); // RSA Decryption
